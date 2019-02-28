2ae80dafe9c54afcde4dda5cfbf8f37de2b404e7
Claudio Melis <claudio.melis@flosslab.com>
2019-02-28 15:18:43 +0100

    add menu search filter input

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
+  TextInput,
   Text,
   View
 } from 'react-native';
@@ -120,17 +121,56 @@ export class ContentView extends Component {
   }
 }
 
-export function Menu({datasource, onItemSelected}) {
+export class Menu extends Component{
 
-  return (
-      <ScrollView scrollsToTop={false} style={styles.menu}>
-        <FlatList
-            data={datasource}
-            renderItem={({item}) => <Text onPress={() => onItemSelected(item)}
-                                          style={styles.item}>{item.key}</Text>}
-        />
-      </ScrollView>)
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      text: "",
+      datasource: props.datasource
+    };
+
+    this.arrayholder = props.datasource;
+  }
+
+  searchFilterFunction = (text: string) => {
+      const newData = this.arrayholder.filter(function(item) {
+        const itemData = item.key.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+
+      this.setState({
+        datasource: newData,
+        text: text
+      });
+  };
+
+  render() {
+    return (
+        <View style={styles.menu}>
+          <TextInput
+              style={styles.textInputStyle}
+              onChangeText={text => this.searchFilterFunction(text)}
+              value={this.state.text}
+              underlineColorAndroid="transparent"
+              placeholder="Search Here"
+          />
+        <ScrollView scrollsToTop={false} >
+          <FlatList
+              data={this.state.datasource}
+              renderItem={({item}) => (
+                  <Text onPress={() => this.props.onItemSelected(item)}
+                                            style={styles.item}>{item.key}</Text>
+              )}
+              enableEmptySections={true}
+              style={{ marginTop: 10 }}
+
+          />
+        </ScrollView>
+        </View>)
+  }
 }
 
 Menu.propTypes = {
@@ -158,5 +198,10 @@ const styles = StyleSheet.create({
   pdf: {
     flex: 1,
     width: Dimensions.get('window').width,
-  }
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    backgroundColor: '#FFFFFF',
+  },
 });
